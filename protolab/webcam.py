e14bb803e33987d93fc2360a679e1fdda69167c2
Alexandre Mazel <alexandre.zelma@gmail.com>
2015-02-03 15:20:18 +0100

    up
    Signed-off-by: Alexandre Mazel <alexandre.zelma@gmail.com>

--- protolab/webcam.py
+++ protolab/webcam.py
@@ -30,17 +30,24 @@ class WebCam():
         im = webcam.getImage();
         cv2.imwrite( "/tmp/test.jpg", im )
     """
-    def __init__( self, strDeviceName = "/dev/video0", nWidth = 640, nHeight = 480 ):
+    def __init__( self, strDeviceName = "/dev/video0", nWidth = 640, nHeight = 480, nNbrBuffer = 1 ):
+        """
+        - nNbrBuffer: put a small number to have short latency a big one to prevent missing frames
+        """
         print( "INF: WebCam: opening: '%s'" % strDeviceName );
         self.video = v4l2capture.Video_device(strDeviceName)
         # Suggest an image size to the device. The device may choose and
         # return another size if it doesn't support the suggested one.
-        size_x, size_y = self.video.set_format(nWidth, nHeight)
-        print( "format is: %dx%d" % (size_x, size_y) );
+        self.size_x, self.size_y = self.video.set_format(nWidth, nHeight)
+        print( "format is: %dx%d" % (self.size_x, self.size_y) );
+        
+        framerate = self.video.set_fps(30); # can't succeed in changing that on my cheap webcam, but work on my computer
+        print( "framerate is: %d" % (framerate) );
+        
         # Create a buffer to store image data in. This must be done before
         # calling 'start' if v4l2capture is compiled with libv4l2. Otherwise
         # raises IOError.
-        self.video.create_buffers(30)
+        self.video.create_buffers(nNbrBuffer) # would be better to play with fps, but it's not working on mine...
         # Send the buffer to the device. Some devices require this to be done
         # before calling 'start'.
         self.video.queue_all_buffers()
@@ -64,7 +71,7 @@ class WebCam():
         #image = Image.fromstring("RGB", (size_x, size_y), image_data)
         #image.save(strFilename)
         
-        nparr = np.fromstring(image_data, np.uint8).reshape( 480,640,3)
+        nparr = np.fromstring(image_data, np.uint8).reshape( self.size_y,self.size_x,3)
         nparr = cv2.cvtColor(nparr, cv2.cv.CV_BGR2RGB);
         return nparr
 # class WebCam - end
